835270b84cdcd3604603a014281dd9aa928a467f
venice <venicepsy@gmail.com>
2024-05-31 16:33:15 +0800

    second commit

--- BdayBuddy App/app/index.tsx
+++ BdayBuddy App/app/index.tsx
@@ -1,56 +1,19 @@
-import { Text, View, SafeAreaView, TouchableOpacity, Image, StyleSheet, Platform, Button } from "react-native";
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import "react-native";
 
-export default function HomeScreen() {
-  function handleLogin() {
-    console.log("login");
-  }
+import AuthHomeScreen from "./auth_screens/homescreen";
 
-  function handleSignUp () {
-    console.log("sign up");
-  }
+import HomeScreen from "./screens/homescreen";
+import LoginScreen from "./screens/login";
+import SignUpScreen from "./screens/signup";
 
-  return (
-    <SafeAreaView className="bg-indigo-300"
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    > 
-       <Image
-          source={require('@/assets/images/bdaybuddy-logo.png')}
-          style={styles.stepContainer}
-        />
-       <Text className="text-4xl font-bold py-3">BDAYBUDDY</Text>
-       <TouchableOpacity className="bg-orange-200 rounded-full px-12 py-2" onPress={handleLogin}>
-         <Text className="text-xl font-bold">Login</Text>
-       </TouchableOpacity>
-       <Text> </Text>
-       <TouchableOpacity className="bg-orange-200 rounded-full px-9 py-2" onPress={handleSignUp}>
-         <Text className="text-xl font-bold">Sign Up</Text>
-       </TouchableOpacity>
-    </SafeAreaView>
-  );
-}
+export default function App() {
+    const loggedIn = false;
+    if (loggedIn) {
+        return <AuthHomeScreen />
+    } else {
+        // return <HomeScreen />
+        // return <LoginScreen />
+        return <SignUpScreen />
+    }
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
-
+};
--- BdayBuddy App/tailwind.config.js
+++ BdayBuddy App/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/*.tsx"],
+  content: ["./app/**/*.tsx"],
   theme: {
     extend: {},
   },
